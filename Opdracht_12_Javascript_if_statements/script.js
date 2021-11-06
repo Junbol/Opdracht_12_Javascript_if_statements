8aebe6ed95ee0b45500feda64696c9efff9a251c
Junier <junierba@yahoo.co.uk>
2021-11-06 18:17:16 +0100

    Add files via upload

---
+++ Opdracht_12_Javascript_if_statements/script.js
@@ -0,0 +1,45 @@
+console.log("Hello Winc Academy students");
+
+// if else statements
+
+const age = 15;
+
+const isFemale = true;
+
+const driverStatus = "bob";
+
+if (age >= 18) {
+  console.log("You can enter");
+} else if (age > 15) {
+  console.log("You are 17 still too young");
+} else {
+  console.log("You are not over 18");
+}
+
+if (isFemale == true) {
+  console.log("Because you are a female so you have a discount");
+} else {
+  console.log("You are not a female");
+}
+
+if (driverStatus == "bob") {
+  console.log("You can drive back home safe");
+} else {
+  console.log("You are too drunk to drive");
+}
+
+/*
+ STORAGE:
+
+ else if (isFemale == true) {
+  console.log("Because you are a female so you have a discount"); 
+  
+}
+
+
+
+ else {
+  console.log("Because you are a male so don't have a discount"); 
+}
+
+*/
